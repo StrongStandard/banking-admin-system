78e012d2c6cbe9d7fcad1b5c4d0d653b1ab12235
Martin Cerveny <163520057+StrongStandard@users.noreply.github.com>
2025-09-17 14:52:39 +0100

    Update BankFunctions.c

--- BankAdmin.c/BankFunctions.c
+++ BankAdmin.c/BankFunctions.c
@@ -1,166 +1,316 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <CommonCrypto/CommonDigest.h>
-#include "BankFunctions.c"
+
+// Maximum number of accounts 
+    #define MAXaccounts 100 
+    #define PASSWORD_LENGTH 15
+    #define NAME_LENGTH 25
+
+// Define a structure to represent a bank account with it's attributes
+    typedef struct 
+    {
+        int accountNumber;
+        char accountHolder[25];
+        float balance;
+        unsigned char passwordHash[CC_SHA256_DIGEST_LENGTH];
+    } BankAccount;
+                                  
+// prototypes
+void createAccount(BankAccount *account, int accountNumber, char accountHolder[], float initialBalance, unsigned char password[]);
+void deposit(BankAccount *account);
+void withdraw(BankAccount *account);
+void checkBalance(BankAccount *account);
+bool checkPassword(BankAccount *account,char inputPassword[PASSWORD_LENGTH]);
+bool checkUser(BankAccount *account, char name[NAME_LENGTH]);
+void saveAccounts(BankAccount *account, int numAccounts);
+int loadAccounts(BankAccount *accounts, int maxAccounts);
+void get_password_from_user(char password[PASSWORD_LENGTH],char passwordCheck[PASSWORD_LENGTH]);
+void get_user_name(char name[NAME_LENGTH]);
+int get_matching_account(BankAccount *accounts, char name[NAME_LENGTH], int numAccounts);
+void sha256_hash(const char *password, unsigned char hash[CC_SHA256_DIGEST_LENGTH]);
 
 
-// Main function
-int main()
+
+// Function to create a new bank account
+
+void createAccount(BankAccount *account, int accountNumber, char accountHolder[], float initialBalance, unsigned char password[])
 {
-    BankAccount accounts[MAXaccounts];
-    int numAccounts = loadAccounts(accounts, MAXaccounts);
-    int account_counter = 0;      
-    int accountNumber = 0;
-    char option = 0; 
-    bool validInput = false; 
-    char name[NAME_LENGTH] = ""; 
-    char password[PASSWORD_LENGTH] = ""; 
-    char passwordCheck[PASSWORD_LENGTH] = "";
-    char inputPassword[PASSWORD_LENGTH];
+    account -> accountNumber = accountNumber; // 'account' represnts BankAccount, '->' represnets pointer to data attribute 'accountNumber' in data structure BankAccount 
+    strcpy(account -> accountHolder, accountHolder);
+    account -> balance = initialBalance;
+    memcpy(account->passwordHash, password, CC_SHA256_DIGEST_LENGTH);
 
-    // global options menu
-    do
-    {  
+}
+
+// Function to deposit money
+
+void deposit(BankAccount *account)
+{
+    float newBalance = 0.00;
+    printf("How much would you like to deposit? \n");
+    scanf("%f", &newBalance);       
+    getchar();
+    account -> balance += newBalance;
+    printf("\nDeposit was succesful, new balance is %.2f£. Press ENTER to continue. \n", account -> balance);
+}
+
+// Function to withdraw money
+
+void withdraw(BankAccount *account)
+{
+    
+    float amount = 0.00;
+    printf("How much would you like to withdraw? \n");
+    scanf("%f", &amount);      
+    getchar();
+    if(amount <= account -> balance)
+    {
+       account -> balance -= amount;
+       printf("\n\nWithdrawal was succesful, new balance is %.2f£. Press ENTER to continue. \n", account -> balance); 
+    }
+    else
+    {
+        printf("\nInsuficient Funds! Press ENTER to continue. \n");
+    }
+    
+}
+// Function to check balance
+
+void checkBalance(BankAccount *account)
+{
+    printf("\nCurrent balance for %s is: %.2f£. Press ENTER to continue. \n", account->accountHolder, account->balance);
+    getchar();
+}
+
+
+// Function to check if the password is correct
+
+bool checkPassword(BankAccount *account,char inputPassword[PASSWORD_LENGTH])
+{
+
+    //printf("Type your password for account %d ", account->accountNumber);
+    char *input_temp_password = getpass("Type your password for account: ");
+    memcpy(inputPassword,input_temp_password,PASSWORD_LENGTH);
+    //scanf("%s", inputPassword);
+
+    // Hash the input password using SHA-256
+
+    unsigned char inputHash[CC_SHA256_DIGEST_LENGTH];
+    sha256_hash(inputPassword, inputHash);
+
+    // Compare the hashed input password with the stored hashed password
+
+    if (memcmp(inputHash, account->passwordHash, CC_SHA256_DIGEST_LENGTH) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// function to save accounts
+
+void saveAccounts(BankAccount *account, int numAccounts)
+{
+    FILE *file = fopen("/Users/martincerveny/BankAdmin/accounts.txt", "w");
+
+    if (file == NULL)
+    {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    for (int i = 0; i < numAccounts; i++)
+    {
+        fprintf(file, "%d,%s,%.2f,", account[i].accountNumber, account[i].accountHolder, account[i].balance);
+
+
+        // Save the hashed password as hexadecimal in the file
+
+        for (int j = 0; j < CC_SHA256_DIGEST_LENGTH; j++)
+        {
+            fprintf(file, "%02x", account[i].passwordHash[j]);
+        }
+
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+// Function to load accounts
+
+int loadAccounts(BankAccount *accounts, int maxAccounts)
+{
+    FILE *file = fopen("/Users/martincerveny/BankAdmin/accounts.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Error opening file for reading.\n");
+        return 0;
+    }
+    
+    int numAccounts = 0;
+    char line[256];
+
+    while (fscanf(file, "%d", &accounts[numAccounts].accountNumber) == 1)
+    {
+        fscanf(file, " %24[^0-9] %f ", accounts[numAccounts].accountHolder, &accounts[numAccounts].balance);
+
+        // Read the hashed password as hexadecimal from the file
+
+        for (int j = 0; j < CC_SHA256_DIGEST_LENGTH; j++)
+        {
+            int scanned = 0;
+            fscanf(file, "%02x", &scanned);
+            accounts[numAccounts].passwordHash[j] = (unsigned char)scanned;
+        }
+
+        // Move to the next line
+
+        fscanf(file, "\n");
+        
+        numAccounts++;
+
+        if (numAccounts >= maxAccounts)
+        {
+            printf("Reached the maximum number of accounts. Some data may be ignored.\n");
+            break;
+        }
+    }
+
+    fclose(file);
+
+    return numAccounts;
+}
+
+// Function to get name from a user
+
+void get_user_name(char name[NAME_LENGTH])
+{
+   int InvalidcharacterCounter = 0;
+   int Spaces = 0;
+            do
+            {
+                InvalidcharacterCounter = 0;
+                Spaces = 0;
+                // Get account name
+                printf("Enter your full name: ");
+                fgets(name, NAME_LENGTH - 1, stdin);
+                name[strlen(name) - 1] = '\0';
+                for (int l = 0; l < strlen(name); l++)
+                {                                      
+                    name[l] = tolower(name[l]);
+                    // printf("%c",name[l]);
+                    if(!isalpha(name[l]) && name[l] != ' ')
+                    {
+                        InvalidcharacterCounter++;
+                    }
+                    if(name[l] == ' ')
+                    {
+                            Spaces++;
+                    }
+                    if(Spaces > 1)
+                    {
+                        InvalidcharacterCounter++;
+                    }
+
+                                                  
+                }  
+                if(InvalidcharacterCounter > 0)
+                {
+                    printf("Invalid characters! Try again. \n");
+                    printf("%i\n", InvalidcharacterCounter);
+                }  
+             
+            }while(InvalidcharacterCounter > 0);
+}
+
+// Function to check if account name is matching
+
+int get_matching_account(BankAccount *accounts, char name[NAME_LENGTH], int numAccounts)
+{
+
+            printf("Enter account holder: ");
+            fgets(name, NAME_LENGTH - 1, stdin);
+            name[strcspn(name, "\n")] = '\0';
+
+            for (int i = 0; i < strlen(name); i++)
+            {
+                name[i] = tolower(name[i]);
+            }
+
+            for(int j = 0; j < numAccounts; j++)
+            {
+                        // If user exists 
+                if(strcmp(name, accounts[j].accountHolder) == 0)
+                {
+                    return j;
+                }
+            //printf("%s/n",accounts[j].accountHolder);
+            }
+            
+            printf("%s/n",name);
+            printf("User not found! Try again.\n");
+
+            return -1; 
+
+}
+
+// Function to get password from user
+
+void get_password_from_user(char password[PASSWORD_LENGTH],char passwordCheck[PASSWORD_LENGTH])
+{
+    bool validInput = false;
+        do
+        {    // Get account password
+            char *password_temp = getpass("Create new password: ");
+            memcpy(password,password_temp,PASSWORD_LENGTH);
+     
+            bool is_at_least_one_uppercase = false ; 
+            for(int p = 0; p < strlen(password); p++)
+            {
+
+                if(isupper(password[p]))
+                {
+                    is_at_least_one_uppercase = true;
+                }
+            }
+            if(strlen(password) < 10 || strlen(password) > 15 || is_at_least_one_uppercase == false)
+            {
+                getchar();
+                printf("\nThe length of a password is 10-15 characters, at least one uppercase and a digit! \n"
+                           "\n");
+            }
+            else
+            {
+                    validInput = true;
+            }
+        } while(validInput == false);
         do
         {
-            printf("\nWelcome in the Strong Bank! How can we help you today ?\n" 
-            "\n"
-            "Select an option:\n" 
-            "\n"
-            "c - create an account\n"
-            "l - login to existing account\n"
-            "e - end\n");
+         
+            char *password_temp2 = getpass("Confirm the password: ");
+            memcpy(passwordCheck, password_temp2, PASSWORD_LENGTH);
 
-        scanf("%1c", &option);
-        option = tolower(option);
-        getchar();      
+        } while(strcmp(passwordCheck, password) != 0);
 
-        if(option == 'c' || option == 'l' || option == 'e')
-        {
-            validInput = true;
-        }
-        else
-        {
-            printf("**************\n" 
-                "Invalid input!\n"
-                "**************\n");
-        }
-        } while (validInput == false);
+}
+// Function to hash a password using SHA-256
 
-        // Initialise actions taken for each option
-        if(option == 'c' )
-        {   // create new bank account
-            const int MAX_ACCOUNT_NUMBER = 999999;       
-            const int MIN_ACCOUNT_NUMBER = 100000;
-            
+void sha256_hash(const char *password, unsigned char hash[CC_SHA256_DIGEST_LENGTH]) 
+{
+    CC_SHA256_CTX sha256;
+    CC_SHA256_Init(&sha256);
+    CC_SHA256_Update(&sha256, password, strlen(password));
+    CC_SHA256_Final(hash, &sha256);
 
-
-            get_user_name(name);
-
-            // Get account number
-            srand(time(0));
-            accountNumber = (rand() % MAX_ACCOUNT_NUMBER) + MIN_ACCOUNT_NUMBER;
-            // Get password from user
-
-            get_password_from_user(password,passwordCheck);
-            int account_index = numAccounts; // index to keep track of current accounts
-
-            // Hash the password using SHA-256
-            sha256_hash(password, accounts[account_index].passwordHash);
-            
-                                                                       
-                      
-            getchar();
-            createAccount(&accounts[account_index], accountNumber, name, 0.00, accounts[account_index].passwordHash);
-            numAccounts++;     
-                            
-
-            saveAccounts(accounts, numAccounts); 
-
-            printf("\nAccount was succesfuly created!\n"
-                "\n"
-                "Account holder: %s \n"
-                "Account number: %i\n"
-                "Balance: %.2f\n"
-                "\nPress enter to continue.", accounts[account_index].accountHolder, accounts[account_index].accountNumber, accounts[account_index].balance);
-    
-        }
-
-        // log in option menu 
-        else if(option == 'l')
-        
-        {  
-             
-                account_counter = get_matching_account(accounts, name, numAccounts);
-                if(account_counter != -1)      
-                {   
-                    BankAccount *account = &accounts[account_counter];
-                    //printf("Type your password for account %d ", account->accountNumber);
-                        do
-                        {
-                            if(checkPassword(account,inputPassword))
-                            {
-                                validInput = true;
-                            }
-                            else
-                            {
-                                printf("Invalid passsword! Try again. \n");
-                                validInput = false;
-                                getchar();
-                            }
-
-                        } while(validInput == false);
-                        
-                            do
-                            {
-                                    do
-                                    {   // user menu  
-                                        getchar();
-                                        printf("\nWelcome back %s, what would you like to do? \n"
-                                        "\nw - withdraw\n" 
-                                        "d - deposit\n"
-                                        "b - check balance\n" 
-                                        "x - exit\n", accounts[account_counter].accountHolder);
-
-                                        scanf("%1c", &option);      
-                                        option = tolower(option);
-                                            
-
-                                        if(option == 'w' || option == 'd' || option == 'b' || option == 'x')
-                                        {
-                                            validInput = true; 
-                                        }
-                                        else
-                                        {
-                                             validInput = false;
-                                        }
-
-                                    } while (validInput == false);
-
-                                    if(option == 'w')
-                                    {
-                                        withdraw(account);
-                                    }
-
-                                    if(option == 'd')
-                                    {
-                                        deposit(account);
-                                    }
-                                    if(option == 'b')
-                                    {
-                                        checkBalance(account);
-                                    }
-                        
-                            } while (option != 'x');                   
-                }
-        }
-        getchar();
-    } while(option != 'e');
-    saveAccounts(accounts, numAccounts); 
-
-    return 0;
 }
